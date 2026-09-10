e73a6bcaed38f2caf5672236049279f3d00f7f1e
Guido Duchi <grdf.1850@gmail.com>
2026-09-10 13:54:49 -0500

    Expose LLD-05 hard-delete authority [skip ci]

--- src/soma/objectives_tasks/__init__.py
+++ src/soma/objectives_tasks/__init__.py
@@ -1,13 +1,19 @@
 """LLD-05 Task and Objective authority."""
 
-from .contracts.objectives_tasks import AcceptedTaskSchedule, TaskMutationResult
+from .contracts.objectives_tasks import AcceptedTaskSchedule, TaskMutationResult, TaskResultRef
+from .queries.hard_delete import TaskHardDeletePreview, TaskHardDeleteQueryService
 from .repositories.tasks import TaskNoStatus, WfmTaskRepository
+from .services.hard_delete import TaskHardDeleteService
 from .services.task_planning import TaskPlanningService
 
 __all__ = [
     "AcceptedTaskSchedule",
+    "TaskHardDeletePreview",
+    "TaskHardDeleteQueryService",
+    "TaskHardDeleteService",
     "TaskMutationResult",
     "TaskNoStatus",
     "TaskPlanningService",
+    "TaskResultRef",
     "WfmTaskRepository",
 ]
