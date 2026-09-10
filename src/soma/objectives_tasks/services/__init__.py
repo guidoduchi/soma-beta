af01d1f0fffaddf390e43aafed44ea898c6065f3
Guido Duchi <grdf.1850@gmail.com>
2026-09-10 13:54:17 -0500

    Export LLD-05 Task hard-delete service [skip ci]

--- src/soma/objectives_tasks/services/__init__.py
+++ src/soma/objectives_tasks/services/__init__.py
@@ -1,5 +1,6 @@
 """LLD-05 Task and Objective application services."""
 
+from .hard_delete import TaskHardDeleteService
 from .task_planning import TaskPlanningService
 
-__all__ = ["TaskPlanningService"]
+__all__ = ["TaskHardDeleteService", "TaskPlanningService"]
