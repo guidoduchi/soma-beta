ebd06b98ee90ce24210698f8ee8c6729a2c79905
Guido Duchi <grdf.1850@gmail.com>
2026-09-11 16:05:51 -0500

    Export selected Task execution input [skip ci]

--- src/soma/objectives_tasks/services/__init__.py
+++ src/soma/objectives_tasks/services/__init__.py
@@ -2,7 +2,13 @@
 
 from .hard_delete import TaskHardDeleteService
 from .retries import TaskRetryService
-from .task_execution import TaskExecutionService
+from .task_execution import SelectedTaskExecutionStart, TaskExecutionService
 from .task_planning import TaskPlanningService
 
-__all__ = ["TaskExecutionService", "TaskHardDeleteService", "TaskPlanningService", "TaskRetryService"]
+__all__ = [
+    "SelectedTaskExecutionStart",
+    "TaskExecutionService",
+    "TaskHardDeleteService",
+    "TaskPlanningService",
+    "TaskRetryService",
+]
