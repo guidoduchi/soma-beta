5c290197f058341cf1ed7d4db352a50d8c8b140d
Guido Duchi <grdf.1850@gmail.com>
2026-09-11 14:21:57 -0500

    Export WFM retry service [skip ci]

--- src/soma/objectives_tasks/services/__init__.py
+++ src/soma/objectives_tasks/services/__init__.py
@@ -1,6 +1,7 @@
 """LLD-05 Task and Objective application services."""
 
 from .hard_delete import TaskHardDeleteService
+from .retries import TaskRetryService
 from .task_planning import TaskPlanningService
 
-__all__ = ["TaskHardDeleteService", "TaskPlanningService"]
+__all__ = ["TaskHardDeleteService", "TaskPlanningService", "TaskRetryService"]
