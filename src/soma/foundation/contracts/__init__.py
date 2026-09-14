0ae49b91ec3cfee7bbc851fa5554ae012cc84972
Guido Duchi <grdf.1850@gmail.com>
2026-09-14 11:08:36 -0500

    Add Foundation durable job contract exports

---
+++ src/soma/foundation/contracts/__init__.py
@@ -0,0 +1,15 @@
+from .foundation import (
+    DurableJobCancellationResult,
+    DurableJobClaim,
+    DurableJobRecoverySummary,
+    DurableJobState,
+    RestoreJobReconciliation,
+)
+
+__all__ = [
+    "DurableJobCancellationResult",
+    "DurableJobClaim",
+    "DurableJobRecoverySummary",
+    "DurableJobState",
+    "RestoreJobReconciliation",
+]
