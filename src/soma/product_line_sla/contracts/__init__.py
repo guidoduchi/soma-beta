d0ae7e2b6db27b637f4b63b716ae9371f4b51467
Guido Duchi <grdf.1850@gmail.com>
2026-09-17 23:56:12 -0500

    Export Product Line SLA contracts

---
+++ src/soma/product_line_sla/contracts/__init__.py
@@ -0,0 +1,13 @@
+from .product_line_sla import (
+    CatalogMutationResult,
+    PolicyRevisionResult,
+    catalog_result_from_execution,
+    policy_result_from_execution,
+)
+
+__all__ = [
+    "CatalogMutationResult",
+    "PolicyRevisionResult",
+    "catalog_result_from_execution",
+    "policy_result_from_execution",
+]
