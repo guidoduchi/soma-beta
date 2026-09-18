26ffbd6f95e03efab1bbb4a5cbc98b0322990b35
Guido Duchi <grdf.1850@gmail.com>
2026-09-18 00:02:38 -0500

    Export Product Line SLA classification service

--- src/soma/product_line_sla/services/__init__.py
+++ src/soma/product_line_sla/services/__init__.py
@@ -1,4 +1,9 @@
 from .catalog import ProductLineSlaCatalogService
+from .classification import ProductLineSlaClassificationService
 from .policy import ProductLineSlaPolicyService
 
-__all__ = ["ProductLineSlaCatalogService", "ProductLineSlaPolicyService"]
+__all__ = [
+    "ProductLineSlaCatalogService",
+    "ProductLineSlaClassificationService",
+    "ProductLineSlaPolicyService",
+]
