631879e2118b2248332d8aeb9cc14d17d8b4f198
Guido Duchi <grdf.1850@gmail.com>
2026-09-18 00:02:44 -0500

    Export SLA classification contracts

--- src/soma/product_line_sla/contracts/__init__.py
+++ src/soma/product_line_sla/contracts/__init__.py
@@ -1,13 +1,19 @@
 from .product_line_sla import (
     CatalogMutationResult,
+    ClassificationMutationResult,
+    ClassificationPreview,
     PolicyRevisionResult,
     catalog_result_from_execution,
+    classification_result_from_execution,
     policy_result_from_execution,
 )
 
 __all__ = [
     "CatalogMutationResult",
+    "ClassificationMutationResult",
+    "ClassificationPreview",
     "PolicyRevisionResult",
     "catalog_result_from_execution",
+    "classification_result_from_execution",
     "policy_result_from_execution",
 ]
