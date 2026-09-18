dd842ff8e9c0b816d7c0c65f9ea2b38a6cf909ba
Guido Duchi <grdf.1850@gmail.com>
2026-09-18 00:32:37 -0500

    Export canonical SLA cohort projection types

--- src/soma/product_line_sla/algorithms/__init__.py
+++ src/soma/product_line_sla/algorithms/__init__.py
@@ -17,3 +17,21 @@ __all__ = [
 from .individual_sla import IndividualSlaCalculator, IndividualSlaResult, IndividualTierResult
 
 __all__ += ["IndividualSlaCalculator", "IndividualSlaResult", "IndividualTierResult"]
+
+from .cohort_state import (
+    CanonicalCohortCalculator,
+    CanonicalCohortMember,
+    CohortKeyParts,
+    CohortMonthProjection,
+    CohortProjection,
+    canonical_month_bounds,
+)
+
+__all__ += [
+    "CanonicalCohortCalculator",
+    "CanonicalCohortMember",
+    "CohortKeyParts",
+    "CohortMonthProjection",
+    "CohortProjection",
+    "canonical_month_bounds",
+]
