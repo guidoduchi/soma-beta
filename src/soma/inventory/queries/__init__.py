93250ddbf0ac32fe45f0ffba259762e88fe907fd
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 03:02:19 -0500

    Export final Inventory query providers

--- src/soma/inventory/queries/__init__.py
+++ src/soma/inventory/queries/__init__.py
@@ -1,12 +1,19 @@
+from .attention_history import InventoryAttentionQueryService, SpareRequestResponseAttention
 from .fault_tags import FaultTagQueryService
 from .previews import InventoryBulkPreviewTarget, InventoryPreviewsQueryService
 from .stock_needs import InventoryNeedsQueryService
-from .task_context import TaskInventoryContextQueryService
+from .task_context import (
+    ObjectiveInventoryContextQueryService,
+    TaskInventoryContextQueryService,
+)
 
 __all__ = [
     "FaultTagQueryService",
+    "InventoryAttentionQueryService",
     "InventoryBulkPreviewTarget",
     "InventoryNeedsQueryService",
     "InventoryPreviewsQueryService",
+    "ObjectiveInventoryContextQueryService",
+    "SpareRequestResponseAttention",
     "TaskInventoryContextQueryService",
 ]
