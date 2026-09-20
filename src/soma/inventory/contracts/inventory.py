a5e9f4a468c5477985d6251fe5325578b3fb4789
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 00:10:44 -0500

    Allow RMA Inventory mutation result refs

--- src/soma/inventory/contracts/inventory.py
+++ src/soma/inventory/contracts/inventory.py
@@ -10,6 +10,9 @@ _ALLOWED_REF_TYPES = frozenset(
     {
         "device_part_unit",
         "local_need_fulfillment",
+        "rma",
+        "rma_assignment",
+        "rma_identifier",
         "spare_need",
         "spare_need_contributor",
         "spare_need_event",
