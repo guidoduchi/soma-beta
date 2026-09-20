fe4de6c0a6e18106f84dca587e2c87ba571a40c7
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 02:50:13 -0500

    Export final Inventory mutation and preview surfaces

--- src/soma/inventory/queries/__init__.py
+++ src/soma/inventory/queries/__init__.py
@@ -1,9 +1,12 @@
 from .fault_tags import FaultTagQueryService
+from .previews import InventoryBulkPreviewTarget, InventoryPreviewsQueryService
 from .stock_needs import InventoryNeedsQueryService
 from .task_context import TaskInventoryContextQueryService
 
 __all__ = [
     "FaultTagQueryService",
+    "InventoryBulkPreviewTarget",
     "InventoryNeedsQueryService",
+    "InventoryPreviewsQueryService",
     "TaskInventoryContextQueryService",
 ]
