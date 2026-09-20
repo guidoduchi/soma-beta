cc9c9697e7665c9e023e5ccfddc88c6a716136f0
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 02:04:49 -0500

    Export Fault Tag read queries

--- src/soma/inventory/queries/__init__.py
+++ src/soma/inventory/queries/__init__.py
@@ -1,7 +1,9 @@
+from .fault_tags import FaultTagQueryService
 from .stock_needs import InventoryNeedsQueryService
 from .task_context import TaskInventoryContextQueryService
 
 __all__ = [
+    "FaultTagQueryService",
     "InventoryNeedsQueryService",
     "TaskInventoryContextQueryService",
 ]
