c4afe40f1be40a8d771ac00519aa06f5fc1c5520
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 01:52:58 -0500

    Export Fault Tag owner service

--- src/soma/inventory/services/__init__.py
+++ src/soma/inventory/services/__init__.py
@@ -1,3 +1,4 @@
+from .fault_tags import InventoryFaultTagService
 from .needs_stock import InventoryNeedsStockService
 
-__all__ = ["InventoryNeedsStockService"]
+__all__ = ["InventoryFaultTagService", "InventoryNeedsStockService"]
