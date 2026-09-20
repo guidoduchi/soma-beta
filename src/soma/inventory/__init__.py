5cd85341476adcecfa8b9763920d737a78554cb4
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 02:50:22 -0500

    Export final Inventory mutation and preview surfaces

--- src/soma/inventory/__init__.py
+++ src/soma/inventory/__init__.py
@@ -1,8 +1,10 @@
 from .contracts.inventory import InventoryMutationResult, InventoryResultRef
+from .services.corrections_bulk import InventoryCorrectionsBulkService
 from .services.fault_tags import InventoryFaultTagService
 from .services.needs_stock import InventoryNeedsStockService
 
 __all__ = [
+    "InventoryCorrectionsBulkService",
     "InventoryFaultTagService",
     "InventoryMutationResult",
     "InventoryNeedsStockService",
