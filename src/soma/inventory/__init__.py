e383a8cf8e253abbd3db9b8b19d4287806a0f9c8
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 03:02:28 -0500

    Export final Inventory cross-packet providers

--- src/soma/inventory/__init__.py
+++ src/soma/inventory/__init__.py
@@ -1,12 +1,16 @@
 from .contracts.inventory import InventoryMutationResult, InventoryResultRef
+from .report_sections import InventoryReportSectionContributor
 from .services.corrections_bulk import InventoryCorrectionsBulkService
 from .services.fault_tags import InventoryFaultTagService
 from .services.needs_stock import InventoryNeedsStockService
+from .services.participants import InventoryTaskDependencyProvider
 
 __all__ = [
     "InventoryCorrectionsBulkService",
     "InventoryFaultTagService",
     "InventoryMutationResult",
     "InventoryNeedsStockService",
+    "InventoryReportSectionContributor",
     "InventoryResultRef",
+    "InventoryTaskDependencyProvider",
 ]
