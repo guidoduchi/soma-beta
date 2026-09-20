bbbd787016772acd35b969e7e790bcb798fd40e9
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 03:02:24 -0500

    Export Inventory Task participant

--- src/soma/inventory/services/__init__.py
+++ src/soma/inventory/services/__init__.py
@@ -1,9 +1,11 @@
 from .corrections_bulk import InventoryCorrectionsBulkService
 from .fault_tags import InventoryFaultTagService
 from .needs_stock import InventoryNeedsStockService
+from .participants import InventoryTaskDependencyProvider
 
 __all__ = [
     "InventoryCorrectionsBulkService",
     "InventoryFaultTagService",
     "InventoryNeedsStockService",
+    "InventoryTaskDependencyProvider",
 ]
