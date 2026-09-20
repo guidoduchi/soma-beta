6b9884e830e1a2b4befd101e91d3fc8c79173237
Guido Duchi <grdf.1850@gmail.com>
2026-09-20 01:12:21 -0500

    Export Task Inventory context query

--- src/soma/inventory/queries/__init__.py
+++ src/soma/inventory/queries/__init__.py
@@ -1,3 +1,7 @@
 from .stock_needs import InventoryNeedsQueryService
 
 __all__ = ["InventoryNeedsQueryService"]
+
+from .task_context import TaskInventoryContextQueryService
+
+__all__ = ["TaskInventoryContextQueryService"]
