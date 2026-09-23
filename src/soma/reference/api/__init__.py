6cfe0b3431e2e137155784530b37edae38b3eb4a
Guido Duchi <grdf.1850@gmail.com>
2026-09-23 02:21:14 -0500

    Export LLD-02 Reference route authority [skip ci]

---
+++ src/soma/reference/api/__init__.py
@@ -0,0 +1,19 @@
+from .routes_reference import (
+    REFERENCE_ROUTE_SPECS,
+    ReferenceHttpResponse,
+    ReferenceRouteAdapter,
+    ReferenceRouteSpec,
+    ResolvedReferenceRoute,
+    build_route_adapter,
+    resolve_reference_route,
+)
+
+__all__ = [
+    "REFERENCE_ROUTE_SPECS",
+    "ReferenceHttpResponse",
+    "ReferenceRouteAdapter",
+    "ReferenceRouteSpec",
+    "ResolvedReferenceRoute",
+    "build_route_adapter",
+    "resolve_reference_route",
+]
